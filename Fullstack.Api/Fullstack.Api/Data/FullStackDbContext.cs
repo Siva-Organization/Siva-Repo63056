de62c2b822f5b6e69358af9257fc6d5dae3842e4
Siva63056 <siva.billudu1@gmail.com>
2023-04-03 16:30:30 +0530

    added

---
+++ Fullstack.Api/Fullstack.Api/Data/FullStackDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Fullstack.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fullstack.Api.Data
+{
+    public class FullStackDbContext: DbContext
+    {
+        public FullStackDbContext(DbContextOptions options): base(options) { }
+
+        public DbSet<Employee> Employees {  get; set; }
+    }
+}
+
